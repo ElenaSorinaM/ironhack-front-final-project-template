77341ca4954b9858450cd77487a7b95fdf34a448
Elena Manea <maneaelenasorina@gmail.com>
2022-09-14 15:57:50 +0200

    footer changed

--- src/assets/components/Footer.vue
+++ src/assets/components/Footer.vue
@@ -1,23 +1,32 @@
 <template>
-  <footer class="relative w-full py-4 bg-emerald-50 text-slate-800
- font-sans">
- <div>
-    <div>
-      <div><img class="w-20" src="../logo.png" alt="logo"></div>
-        <div><h3>Mi planta feliz</h3></div>
-        <div><p>Copyright @ 2022 Miplantafeliz, SL</p></div>
-        <div><p>Barcelona</p></div>
-    </div>
- </div>
+  <footer class="relative w-full py-4 bg-emerald-50 text-slate-800 font-sans">
+    
+      <div class="flex flex-row flex-wrap justify-between items-center content-center ">
+        <div class="mx-10 px-10">
+          <img class="logo" src="../logo.png" alt="logo" />
+        </div>
+        <div class="grid grid-col-3 gap-0 mx-10 px-10 font-sans">
+          <div>
+            <h3>Mi planta feliz</h3>
+          </div>
+          <div>
+            <p>Copyright @ 2022 Miplantafeliz, SL</p>
+          </div>
+          <div>
+            <p>Barcelona</p>
+          </div>
+        </div>
+      </div>
+    
   </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+  .logo{
+    width: 100px;
+  }
 </style>
