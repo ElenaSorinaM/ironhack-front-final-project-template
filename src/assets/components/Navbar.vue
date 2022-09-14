8b0e22d8bc17be315db5e985b7d149c4fcb9b51b
Elena Manea <maneaelenasorina@gmail.com>
2022-09-14 15:42:11 +0200

    navbar fixed

--- src/assets/components/Navbar.vue
+++ src/assets/components/Navbar.vue
@@ -1,56 +1,55 @@
 <template>
-<nav class="relative w-full flex flex-wrap items-center justify-between
- bg-emerald-700 text-slate-50 hover:text-slate-300 focus:text-slate-400 shadow-lg
- font-sans">
-    <div class="w-full flex flex-wrap items-center justify-between px-6"> 
-        <div class="flex-grow items-center"> 
-            <div><img class ="logopicture" src="../logo.png" alt="logo"></div>
+  <nav
+    class="relative flex flex-row flex-wrap  justify-between items-center
+    bg-emerald-700"
+  >   
+    <div class="m-5 text-slate-50 hover:text-slate-300 focus:text-slate-400">
+      <h3 class="text-xl font-semibold font-sans cursor-pointer">Mi planta feliz</h3>
     </div>
-    <div >
-        <ul class="flex flex-col pl-0 list-style-none mr-auto">
-            <li class="p-2 text-xl text-slate-50 hover:text-slate-300 focus:text-slate-400" @click="logOutOk">
-                <p class="cursor-pointer">Logout</p>
-            </li>
-        </ul>
+
+    <div class="m-5">
+      <ul>
+        <li
+          class="text-xl font-sans text-slate-50 hover:text-slate-300 focus:text-slate-400"
+          @click="logOutOk"
+        >
+          <p class="cursor-pointer">Logout</p>
+        </li>
+      </ul>
     </div>
-    </div>
-</nav>
-<div v-if="error" class=" text-red-600"
-            role="alert">
-            <span>{{ this.errorMessage }}</span></div>
+  </nav>
+  <div v-if="error" class="text-red-600" role="alert">
+    <span>{{ this.errorMessage }}</span>
+  </div>
 </template>
 
 <script>
 import { useUserStore } from "../../store/user";
 
 export default {
-    name: "navbar", 
-    setup(){
-        const user = useUserStore();
-        return { user };
-},
- data() {
-        return {
-            error: false,
-            errorMessage: "",
-        }
-},
-methods: {
+  name: "navbar",
+  setup() {
+    const user = useUserStore();
+    return { user };
+  },
+  data() {
+    return {
+      error: false,
+      errorMessage: "",
+    };
+  },
+  methods: {
     async logOutOk() {
-    try {
+      try {
         await this.user.signOut();
         this.$router.go();
-    } catch (e) {
-        this.error=true;
-        this.errorMessage = "Oops ha passado algo!"
-    }
-}, 
-},
+      } catch (e) {
+        this.error = true;
+        this.errorMessage = "Oops ha passado algo!";
+      }
+    },
+  },
 };
 </script>
 
-<style>
-.logopicture{
-    width: 80px;
-}
-</style>
+<style></style>
